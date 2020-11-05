6690868625845d983a083762072c5b23629bb2e3
Edgar Luque <edgar.rll@gmail.com>
2020-11-05 18:37:15 -0500

    Agregue el printScreen

--- src/holaMundo/pkg/holaUno.java
+++ src/holaMundo/pkg/holaUno.java
@@ -2,7 +2,7 @@ package holaMundo.pkg;
 
 public class holaUno {
     public static void main (String[]args){
-        System.out.println("  ¡Hola mundo!  ");
+        System.out.println("  ¡Hola mundo! Yes Yes!! ");
     }
 
 }
