4d6a7b58dcff7cd2206b88103ea2813b447e285a
Yesenia Oliva <56052112+YeseniaGpe@users.noreply.github.com>
2020-11-05 17:48:04 -0600

    Hola

--- src/holaMundo/pkg/holaUno.java
+++ src/holaMundo/pkg/holaUno.java
@@ -3,6 +3,7 @@ package holaMundo.pkg;
 public class holaUno {
     public static void main (String[]args){
         System.out.println("  ¡Hola mundo! Yes Yes!! ");
+        System.out.println(" Hola 2");
     }
 
 }
