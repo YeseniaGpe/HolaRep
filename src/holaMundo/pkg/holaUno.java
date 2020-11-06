6939b062c3b6d0cb14afd60722c92d5207054fa5
Edgar <edgar.rll@gmail.com>
2020-11-05 21:42:37 -0500

    Agregue el printScreen

--- src/holaMundo/pkg/holaUno.java
+++ src/holaMundo/pkg/holaUno.java
@@ -4,6 +4,7 @@ public class holaUno {
     public static void main (String[]args){
         System.out.println("  ¡Hola mundo! Yes Yes!! ");
         System.out.println(" Hola 2");
+        System.out.println("A darle!! Éxito!! Feliz día!!");
     }
 
 }
